7127679e61b2cd03e293d9e1baa1f6a1ee2217f0
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-03 23:21:54 +0000

    Automatically backed up by Learn

--- db/migrate/20190302212945_create_dogs.rb
+++ db/migrate/20190302212945_create_dogs.rb
@@ -1,9 +1,20 @@
 class CreateDogs < ActiveRecord::Migration
+<<<<<<< HEAD
   def change
+=======
+  def up
+>>>>>>> f8aace1ee1f237c9080acc645d43123093843217
     create_table :dogs do |t| 
       t.string :name 
       t.string :breed
     end 
   end
+<<<<<<< HEAD
   #the down (droptable) is implied in the change method. 
+=======
+  
+  def down
+    drop_table :dogs 
+  end 
+>>>>>>> f8aace1ee1f237c9080acc645d43123093843217
 end
